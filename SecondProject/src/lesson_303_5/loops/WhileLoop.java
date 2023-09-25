04d47bba7d5700eb1057c882354e0807fe65b6ee
ElviraKh1 <vira2015@aol.com>
2023-09-25 15:16:47 -0400

    practice assigment 303.5.2

---
+++ SecondProject/src/lesson_303_5/loops/WhileLoop.java
@@ -0,0 +1,15 @@
+package lesson_303_5.loops;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean cond = true;
+		while (cond) {
+			System.out.println("Will run forever");
+			cond = false;
+		}
+
+	}
+
+}
